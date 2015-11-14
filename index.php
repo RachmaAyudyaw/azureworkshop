4901fecd7ab47a0c644896230a7140d291dff445
RachmaAyudyaw <rachmaayudyaw1@gmail.com>
2015-11-15 06:17:49 +0700

    initial commit

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <html>
 <head>
-		<title>Web App Sederhana</tittle>
+		<title>Web App Sederhana</title>
 </head>
-	<form acton="hello.php" method="POST">
+<body>
+	<form action="hello.php" method="POST">
 		Nama : <input type="text" name="name"/>
 		<br>
 		Email : <input type="email" name="email"/>
 		<br>
 		<input type="submit" value="Enter"/>
 	</form>
+</body>
 </html>
